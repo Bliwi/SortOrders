106a5689079a91f7a413e8a9ad4694b7aa72ad45
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 17:56:49 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,10 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Active sort orders'] = 'סדרות מיון פעילות';
+$lang['Random'] = 'אקראי';
+$lang['SortOrders'] = 'סדרי מיון';
